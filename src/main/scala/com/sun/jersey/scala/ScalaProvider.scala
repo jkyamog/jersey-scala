d7d1d59e0c03cb3fad2da2cd562101c3adcb46f8
Jun Yamog <jkyamog@gmail.com>
2013-04-30 16:59:47 +1200

    initial commit, just a scala provider for jersey

---
+++ src/main/scala/com/sun/jersey/scala/ScalaProvider.scala
@@ -0,0 +1,66 @@
+package com.sun.jersey.scala
+
+import javax.ws.rs.ext.Provider
+import com.sun.jersey.core.provider.AbstractMessageReaderWriterProvider
+import java.lang.reflect.Type
+import java.lang.annotation.Annotation
+import java.io.{IOException, InputStream, OutputStream}
+import javax.ws.rs.{WebApplicationException, Consumes, Produces}
+import javax.ws.rs.core.{Response, MultivaluedMap, MediaType}
+import javax.ws.rs.core.Response.Status
+import scala.reflect.Manifest
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.core.JsonProcessingException
+
+/**
+ * This provider enables scala objects to be read and written by jersey.  This uses jackson's scala module
+ * This code is heavily borrowed/copied from codahale's jersey scala code.
+ */
+
+@Provider
+@Produces(Array(MediaType.APPLICATION_JSON))
+@Consumes(Array(MediaType.APPLICATION_JSON))
+class ScalaProvider[A] extends AbstractMessageReaderWriterProvider[A] {
+
+  private val jsonMapper = new ObjectMapper with ScalaObjectMapper
+  jsonMapper.registerModule(DefaultScalaModule)
+	
+  def readFrom(klass: Class[A],
+               genericType: Type,
+               annotations: Array[Annotation],
+               mediaType: MediaType,
+               httpHeaders: MultivaluedMap[String, String],
+               entityStream: InputStream) = {
+    try {
+      jsonMapper.readValue(entityStream)(Manifest.classType(klass))
+    } catch {
+      case e: JsonProcessingException => {
+        throw new WebApplicationException(Response.status(Status.BAD_REQUEST)
+          .entity(e.getMessage)
+          .build)
+      }
+    }
+  }
+
+  def isReadable(klass: Class[_],
+                 genericType: Type,
+                 annotations: Array[Annotation],
+                 mediaType: MediaType) = mediaType.isCompatible(MediaType.APPLICATION_JSON_TYPE)
+
+  def writeTo(t: A,
+              klass: Class[_],
+              genericType: Type,
+              annotations: Array[Annotation],
+              mediaType: MediaType,
+              httpHeaders: MultivaluedMap[String, AnyRef],
+              entityStream: OutputStream) {
+    jsonMapper.writeValue(entityStream, t)
+  }
+
+  def isWriteable(klass: Class[_],
+                  genericType: Type,
+                  annotations: Array[Annotation],
+                  mediaType: MediaType) = mediaType.isCompatible(MediaType.APPLICATION_JSON_TYPE)
+}
